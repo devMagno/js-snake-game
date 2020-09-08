d3967931911d51f249db37cb56f364540ace5be9
Guilherme Magno <guilhermertfmagno@gmail.com>
2020-09-08 14:58:42 -0300

    Adiciona movimentação da cobrinha

--- script.js
+++ script.js
@@ -20,6 +20,14 @@ function createSnake() {
   }
 } 
 
+document.addEventListener('keydown', update);
+function update(event) {
+  if (event.keyCode == 37 && direction != "right"){direction = "left"}
+  if (event.keyCode == 39 && direction != "left"){direction = "right"}
+  if (event.keyCode == 38 && direction != "down"){direction = "up"}
+  if (event.keyCode == 40 && direction != "up"){direction = "down"}
+}
+
 function startGame() {
   createBackground()
   createSnake()
