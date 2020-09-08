c98e51a3e72bdf609fc28946ea47cd4b1bc0821a
Guilherme Magno <guilhermertfmagno@gmail.com>
2020-09-08 16:39:56 -0300

    Atualiza fim de jogo

--- script.js
+++ script.js
@@ -51,7 +51,8 @@ function startGame() {
   for(i = 1; i < snake.length; i++){
     if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
       clearInterval(game)
-      alert('GAME OVER! :(')
+      alert('Fim de jogo!\nClique para jogar novamente.')
+      location.reload()
     }
   }
 
