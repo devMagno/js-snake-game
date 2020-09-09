d00958c4d93659df03ca1e3d3278c1af97362e98
Guilherme Magno <guilhermertfmagno@gmail.com>
2020-09-09 13:04:34 -0300

    Corrige teletransporte da cobrinha pelo canvas

--- script.js
+++ script.js
@@ -43,10 +43,10 @@ function update(event) {
 }
 
 function startGame() {
-  if(snake[0].x > 15 * box && direction == "right"){snake[0].x = 0}
-  if(snake[0].x < 0 * box && direction == "left"){snake[0].x = 16 * box}
-  if(snake[0].y > 15 * box && direction == "down"){snake[0].y = 0}
-  if(snake[0].y < 0 * box && direction == "up"){snake[0].y = 16 * box}
+  if(snake[0].x > 15 * box){snake[0].x = 0}
+  if(snake[0].x < 0 * box){snake[0].x = 16 * box}
+  if(snake[0].y > 15 * box){snake[0].y = 0}
+  if(snake[0].y < 0 * box){snake[0].y = 16 * box}
 
   for(i = 1; i < snake.length; i++){
     if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
